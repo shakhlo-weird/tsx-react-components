a573ca6ac61e927e44c6d79c3ad6f2dfc5331173
loalavova <shakhlo@weird.io>
2024-09-23 10:27:27 +0200

    all

---
+++ src/views/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from "./Layout";
+
+
+const Home = () => {
+    return (
+        <Layout>
+            <></>
+        </Layout>
+
+    )
+
+}
+
+export default Home;
